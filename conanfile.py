c87c74d459159f2fac05c613b370e04ba375c41f
Matthew Russell <mrussell@neptec.com>
2018-02-05 11:26:56 -0500

    Removing absolte eigen paths from final CeresConfig.cmake

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-import os
+import os, re
 from conans import ConanFile, CMake, tools
 
 
@@ -49,7 +49,6 @@ class CeresSolverConan(ConanFile):
         args.append('-DCXX11:BOOL=On')
         args.append('-DNO_CMAKE_PACKAGE_REGISTRY:BOOL=On')
         args.append('-DEIGEN_INCLUDE_DIR:PATH=%s'%os.path.join(self.deps_cpp_info['eigen'].rootpath, 'include', 'eigen3'))
-        args.append('-DEigen3_DIR:PATH=%s'%os.path.join(self.deps_cpp_info['eigen'].rootpath, 'share', 'eigen3', 'cmake'))
         args.append('-Wno-dev')
 
         cmake = CMake(self)
@@ -57,10 +56,43 @@ class CeresSolverConan(ConanFile):
         cmake.build()
         cmake.install()
 
+        version_major = int(self.version.split('.')[1])
+        if version_major < 13:
+            path = os.path.join('share', 'Ceres')
+        else:
+            path = os.path.join('lib', 'cmake', 'Ceres')
+        self.fixFindPackage(os.path.join(self.package_folder, path, 'CeresConfig.cmake'))
+
+    def fixFindPackage(self, path):
+        """ Remove absolute paths from the CMake file """
+
+        if not os.path.exists(path):
+            self.output.warn(f'Cannot find CMake find script for Ceres-Solver: {path}')
+            return
+
+        with open(path) as f: data = f.read()
+
+        regex = r'Eigen[^\s]+ (?P<base>.*.data.eigen.(?P<version>\d+.\d+.\d+).*?[a-z0-9]{40})'
+        m = re.search(regex, data, re.IGNORECASE)
+        if not m:
+            self.output.warn(f'Cannot find absolute path to Eigen in CMake find script for Ceres-Solver: {path}')
+            return
+
+        data = data.replace(m.group('base'), '${CONAN_EIGEN_ROOT}')
+        data = data.replace(m.group('version'), self.deps_cpp_info['eigen'].version)
+
+        with open(path, 'w') as f: f.write(data)
+
     def package(self):
         pass
 
     def package_info(self):
-        pass
+        # The CMake files move in different versions, so we're going to use the
+        # resdir to point to these.
+        version_major = int(self.version.split('.')[1])
+        if version_major < 13:
+            self.cpp_info.resdirs.append(os.path.join('share', 'Ceres'))
+        else:
+            self.cpp_info.resdirs.append(os.path.join('lib', 'cmake', 'Ceres'))
 
 # vim: ts=4 sw=4 expandtab ffs=unix ft=python foldmethod=marker :
