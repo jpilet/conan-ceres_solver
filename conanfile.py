3012034edd8675e2c382acc0fcf98bc01d8e4b4d
Matthew Russell <mrussell@neptec.com>
2018-07-31 22:08:22 -0400

    fixup! fixup! Protecting options check on Windows

--- conanfile.py
+++ conanfile.py
@@ -148,17 +148,17 @@ class CeresSolverConan(ConanFile):
         else:
             libext = 'lib'
 
-        if self.options.getattr('blas') == 'custom':
+        if getattr(self.options, 'blas') == 'custom':
             cmake.definitions['CUSTOM_BLAS:BOOL'] = 'ON'
         else:
             cmake.definitions['CUSTOM_BLAS:BOOL'] = 'OFF'
 
-            if self.options.getattr('blas') == 'openblas' and 'openblas' in self.deps_cpp_info.deps:
+            if getattr(self.options, 'blas') == 'openblas' and 'openblas' in self.deps_cpp_info.deps:
                 libopenblas = os.path.join(self.deps_cpp_info['openblas'].rootpath, self.deps_cpp_info['openblas'].libdirs[0], f'libopenblas.{libext}')
                 cmake.definitions['BLAS_openblas_LIBRARY:FILEPATH']   = libopenblas
                 cmake.definitions['LAPACK_openblas_LIBRARY:FILEPATH'] = libopenblas
 
-            if self.options.getattr('blas') == 'blas' and 'lapack' in self.deps_cpp_info.deps:
+            if getattr(self.options, 'blas') == 'blas' and 'lapack' in self.deps_cpp_info.deps:
                 libdir = os.path.join(self.deps_cpp_info['lapack'].rootpath, self.deps_cpp_info['lapack'].libdirs[0])
                 cmake.definitions['BLAS_blas_LIBRARY:FILEPATH']     = os.path.join(libdir, f'libblas.{libext}')
                 cmake.definitions['LAPACK_lapack_LIBRARY:FILEPATH'] = os.path.join(libdir, f'liblapack.{libext}')
